4c540d3e927ee92d151911b0a261376296f458e0
garryvar <darisvet.sokolov@yandex.ru>
2024-09-11 17:49:20 +0300

    update graphic and some fixes

--- src/components/Graphics/Graphics.jsx
+++ src/components/Graphics/Graphics.jsx
@@ -1,13 +1,64 @@
 import { Component } from "react";
 import style from "./Graphics.module.css";
-import pic1 from "../../img/graphics/hockey_club.jpg";
+import pic1 from "../../img/graphics/hockey_club.webp";
 import pic2 from "../../img/graphics/martian_chronicles.jpg";
 import pic3 from "../../img/graphics/saturn_records.jpg";
-import pic4 from "../../img/graphics/pie_cosmo_cacke.jpg";
-import pic5 from "../../img/graphics/wash_your_car.jpg";
+import pic4 from "../../img/graphics/pie_cosmo_cacke.webp";
+import pic5 from "../../img/graphics/wash_your_car.webp";
 import pic6 from "../../img/graphics/shankey_vintage.jpg";
-import pic7 from "../../img/graphics/ufo.jpg";
-import pic8 from "../../img/graphics/red_eyes_totem.jpg";
+import pic7 from "../../img/graphics/ufo.webp";
+import pic8 from "../../img/graphics/red_eyes_totem.webp";
+
+const graphicsItemsData = [
+    {
+        image: pic1,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic2,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic3,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic4,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic5,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic6,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic7,
+        width: '180',
+        height: '180',
+        
+    },
+    {
+        image: pic8,
+        width: '180',
+        height: '180',
+        
+    },
+]
 
 
 export default class Graphics extends Component {
@@ -23,44 +74,12 @@ export default class Graphics extends Component {
                 </div>
                 <p className="subtitle animate__animated animate__fadeIn">Мои идеи реализованые в работах с 2d графикой и дизайном</p>
                 <div className={style.grid}>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic1} alt="" width="180" height="180" />
-                    </div>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic2} alt="" width="180" height="180" />
-                    </div>
-
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                    <   img src={pic3} alt="" idth="180" height="180" />
-                    </div>
-
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic4} alt="" idth="180" height="180" />
-                    </div>
-
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic5} alt="" idth="180" height="180" />
-                    </div>
-
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic6} alt="" idth="180" height="180" />
-                    </div>
-
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic7} alt="" idth="180" height="180" />
-                    </div>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src={pic8} alt="" idth="180" height="180" />   
-                    </div>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src="" alt="" />
-                    </div>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src="" alt="" />
-                    </div>
-                    <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
-                        <img src="" alt="" />
-                    </div>
+                    {
+                        graphicsItemsData.map(({image, width, height}) => 
+                            <div className={`${ style.gridItem} animate__animated animate__fadeInUp`}>
+                                <img src={image} alt={'rt'} width={width} height={height} />
+                            </div>)
+                    }
                 </div>
             </div>
         )
