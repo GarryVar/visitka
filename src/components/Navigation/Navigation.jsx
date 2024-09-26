346be76a74ce9ae8609b4a23cb0df15ed67b7ddc
garryvar <darisvet.sokolov@yandex.ru>
2024-09-26 14:35:07 +0300

    Added svg icons to navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -9,14 +9,17 @@ export default class Navigation extends Component {
             <div className={this.props.menuVisible ? style.nav : style.navHidden}>
                 <ul className={style.menu}>
                     {
-                        this.props.menuItemsData.map(({textContent, link}, indx) =>
+                        this.props.menuItemsData.map(({textContent, link, icon}, indx) =>
                             <li className={style.item} key={indx}>
+                                <div className={style.icon}>
+                                    {icon}
+                                </div>
                                 <NavLink
                                     title={textContent}
                                     onClick={this.props.hideNavMenu}
-                                    to={link} 
-                                    className={({isActive}) => `${style.link} ${( isActive ? ' active' : '')}`}>
-                                    
+                                    to={link}
+                                    className={({isActive}) => `${style.link} ${(isActive ? ' active' : '')}`}>
+
                                     {textContent}
                                 </NavLink>
                             </li>
--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -1,21 +1,33 @@
+import { ReactSVG } from "react-svg";
+
+import iconUser from "../images/icon-user.svg";
+import iconDev from "../images/icon-code.svg";
+import iconImages from "../images/icon-images.svg";
+import iconMusic from "../images/icon-music.svg";
+
+
 export const menuItemsData = [
     {
         textContent:'Про меня',
+        icon: <ReactSVG src={ iconUser } />,
         link: '/'
     },
 
     {
         textContent:'Разработка',
+        icon: <ReactSVG src={ iconDev } />,
         link: '/dev'
     },
 
     {
         textContent:'Моя Музыка',
+        icon: <ReactSVG src={ iconMusic } />,
         link: '/music'
     },
 
     {
         textContent:'Графика',
+        icon: <ReactSVG src={ iconImages } />,
         link: '/graphics'
     },
 ];
